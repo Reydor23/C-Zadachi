eb1f6f94000361e24003f1d6dac68dbe1d68b406
Reydor23 <109434298+Reydor23@users.noreply.github.com>
2024-02-17 13:04:56 +0200

    Adding 2 zadachi

---
+++ ZadachiCsharp/Zadacha2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Security.Cryptography;
+
+namespace Zadacha2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double redRoses = 7.89;
+            double tulips = 5.89;
+            double orchid = 45.32;
+            int arrangingaBouquet = 5;
+            Console.WriteLine("Price of the flowers: ");
+            Console.WriteLine($"1.Red Roses: {redRoses}lv\n2.Tulips: {tulips}lv\n3.Orchid: {orchid}lv");
+            Console.WriteLine($"Arranging a Bouquet: {arrangingaBouquet}lv");
+            
+            Console.Write("How much Red Roses: ");
+            int rosesCounter = int.Parse(Console.ReadLine());
+            Console.Write("How much Tulips: ");
+            int tulipsCounter = int.Parse(Console.ReadLine());
+            Console.Write("How much Orchid: ");
+            int orchidCounter = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Choose season:\n1.Spring\n2.Summer");
+            Console.Write("Choose Season: ");
+            int season = int.Parse(Console.ReadLine());
+
+            int totalFlowers = rosesCounter + tulipsCounter + orchidCounter;
+
+            double total = 0;
+            double valentineSale = 0.15;
+
+            switch (season) 
+            {
+                case 1:
+                    total = (rosesCounter * redRoses) + (tulipsCounter * tulips) + (orchidCounter * orchid) + arrangingaBouquet * valentineSale;
+                    if (tulipsCounter >= 3)
+                    {
+                        total *= 0.95;
+                    }            
+                    Console.WriteLine($"Total amount during spring season: {total}");
+                    break;
+                    case 2:
+                    total = (rosesCounter * redRoses) + (tulipsCounter * tulips) + (orchidCounter * orchid) + arrangingaBouquet * valentineSale;
+                    if (rosesCounter >=12)
+                    {
+                        total *= 0.90;
+                    }
+                    Console.WriteLine($"Total amount during summer season: {total}");
+                    break;
+
+            }
+            if (totalFlowers > 22)
+            {
+                total *= 0.77;
+                string formattedNumber = total.ToString("0.00");
+                Console.WriteLine($"Total amount: {formattedNumber}");
+            }
+
+        }
+    }
+}
---
+++ ZadachiCsharp/ZadachiCsharp/Zadacha1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ZadachiCsharp
+{
+    internal class Program
+   {
+      static void Main(string[] args)
+       {
+           double vipTickets = 497.54;
+           double normalTickets = 245.80;
+           Console.WriteLine($"The price of VIP tickets: {vipTickets}");
+           Console.WriteLine($"The price of Normal tickets: {normalTickets}");
+
+           Console.Write("Write your budged: ");
+           double budged = double.Parse(Console.ReadLine());
+
+           Console.Write("How many people are you going to: ");
+           int people = int.Parse(Console.ReadLine());
+           double budgedWithDiscount = 0;
+
+           if (people >= 1 && people <= 4)
+           {
+               budgedWithDiscount = budged * 0.75;
+           }
+           else if (people >= 5 && people <= 9)
+           {
+               budgedWithDiscount = budged * 0.60;
+
+           }
+           else if (people >= 10 && people <= 24)
+           {
+               budgedWithDiscount = budged * 0.50;
+           }
+           else if (people >= 25 && people <= 49)
+           {
+               budgedWithDiscount = budged * 0.40;
+           }
+           else if (people >= 50)
+           {
+               budgedWithDiscount = budged * 0.25;
+           }
+           else
+           {
+               Console.WriteLine("There's no people!");
+           }
+           Console.WriteLine($"Your left budged: {budgedWithDiscount}");
+
+           if (budgedWithDiscount > vipTickets)
+           {
+               Console.WriteLine("You can buy VIP tickets.");
+           }
+           if (budgedWithDiscount > normalTickets)
+           {
+               Console.WriteLine("You can buy Normal tickets.");
+           }
+       }
+   }
+}
