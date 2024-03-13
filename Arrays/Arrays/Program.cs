5848827e67f412612e6c396d602b531a5b1f15be
Reydor23 <109434298+Reydor23@users.noreply.github.com>
2024-03-13 17:21:31 +0200

    Add new exes

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("All Months:");
+            string[,] months = { 
+                { "1.January", "2.February", "3.March", "4.April", "5.May" ,"6.June" },
+                { "7.July", "8.August","9.September", "10.Octomber", "11.November" , "12.December"}
+            };
+            foreach (string month in months) 
+            {
+                Console.WriteLine(month);
+            }
+            Console.WriteLine(" ");
+            Console.Write("Enter year: ");
+            int year = int.Parse(Console.ReadLine());
+
+            //if(DateTime.IsLeapYear(year))
+            if (year % 4 ==0)
+            {
+                Console.WriteLine("The year is leap!");
+                Console.WriteLine(" ");
+                string[,] daysLeapYear = {
+               { "31 days in Jan", "29 days in Feb", "31 days in March", "30 in April", "31 days in May" ,"30 days in June" },
+                { "31 days in July", "31 days in August","30 days in September", "31 days in Octomber", "30 days in November" , "31 days in December"}
+            };
+                foreach (string day in daysLeapYear)
+                {
+                    Console.WriteLine(day);
+                }
+            }
+            else 
+            {
+                Console.WriteLine("The year is not leap!");
+                Console.WriteLine(" ");
+
+                string[,] daysNormal = {
+               { "31 days in Jan", "28 days in Feb", "31 days in March", "30 in April", "31 days in May" ,"30 days in June" },
+                { "31 days in July", "31 days in August","30 days in September", "31 days in Octomber", "30 days in November" , "31 days in December"}
+            };
+                foreach (string dayN in daysNormal)
+                {
+                    Console.WriteLine(dayN);
+                }
+            }
+                
+                    
+               
+                   
+                   
+            
+
+            
+        }
+    }
+}
---
+++ Arrays/Meterologiya/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Meterologiya
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namesOftheStations = { "Novo Selo", "Vidin", "Lom", "Oryahovo", "Montana", "Vraca", "Kneja", "Lovech", "Pleven" };
+            int[] temperatures = { 8, 8, 8, 8, 15, 14, 8, 10, 13 };
+            Console.WriteLine("All Stations: ");
+            for (int i = 0; i < namesOftheStations.Length; i++)
+            {
+                Console.WriteLine($"{namesOftheStations[i]} : {temperatures[i]}T°C");
+
+            }
+
+            for (int i = 0; i < temperatures.Length; i++)
+            {
+                if (i >= 8 || i >= 15)
+                {
+                    Console.WriteLine($"Nai-malka stoinost: {i}");
+                    
+                }       
+            }
+            
+            
+        }
+    }
+}
