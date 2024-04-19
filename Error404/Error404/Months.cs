31479901822aebbd7156c77b5def1e45c06c0c5a
Reydor23 <109434298+Reydor23@users.noreply.github.com>
2024-04-19 21:52:01 +0300

    Novi Zad

---
+++ Error404/Error404/Months.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Error404
+{
+    public class Months
+    {
+        public int MonthCon(string monthName)
+        {
+            DateTimeFormatInfo formatInfo = new DateTimeFormatInfo();
+            DateTime date = DateTime.ParseExact(monthName, "MMMM", CultureInfo.CurrentCulture);
+            
+            return date.Month;
+        }
+    }
+}
---
+++ Error404/Error404/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Error404
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                Months selectMonths = new Months();
+
+                Console.Write("Enter month: ");
+                string month = Console.ReadLine();
+
+                int monthNumber = selectMonths.MonthCon(month);
+
+                Console.WriteLine($"The number of the month {month} is: {monthNumber}");
+        }
+    }
+}
