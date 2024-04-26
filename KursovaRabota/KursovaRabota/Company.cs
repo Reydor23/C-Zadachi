2b7a5e8805af64c80f1a2f27de3b23c8f339d590
Reydor23 <109434298+Reydor23@users.noreply.github.com>
2024-04-27 02:06:01 +0300

    Add Cursova Rabota

---
+++ KursovaRabota/KursovaRabota/Company.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KursovaRabota
+{
+    internal class Company
+    {
+        private string nameCompany, companyBulstat;
+        private DateTime dateOfCompanyCreation;
+
+        protected string nameOfOwner;
+        protected float firstCapital, actualCapital;
+
+       
+        public string NameCompany 
+        { 
+            get { return nameCompany; } 
+            set { nameCompany = value; } 
+        }
+        public DateTime DateOfCompanyCreation
+        {
+            get { return DateOfCompanyCreation; }
+            set { DateOfCompanyCreation = value; }
+        }     
+
+        public string CompanyBulstat
+        { 
+            get { return companyBulstat; } 
+            set {
+
+                if (value.Length <= 10) 
+                { 
+                    companyBulstat = value;
+                    Console.WriteLine("Succesfull Bulstat!");
+                }
+                else
+                {
+                    throw new ArgumentException("Bulstat must be 10 characters!");
+                   
+                }
+            
+            }                 
+        }
+        public void Input()
+        {
+            Console.WriteLine("Enter bulstat: ");
+            string buls = Console.ReadLine();
+
+            try
+            {
+                CompanyBulstat = buls;
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine();
+                throw;
+            }          
+
+        }
+
+
+    }
+}
---
+++ KursovaRabota/KursovaRabota/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace KursovaRabota
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Company company = new Company();
+            company.Input();
+
+            Console.Write("Enter owners name (enter done to stop): ");
+            string input;
+
+            List<string> list = new List<string>();
+
+            do
+            {
+                input = Console.ReadLine();
+                if (input.ToLower() != "done")
+                {
+                    list.Add(input);
+                    Console.Write("New name: ");
+                }
+            }
+
+            while (input.ToLower() != "done");
+            Console.WriteLine("Names of the owners: ");
+
+            foreach (string item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
